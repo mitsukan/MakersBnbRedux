f9728c9d4a0bdc9ecf4d8e8c008a2202a61b7026
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-27 13:24:31 +0100

    bootstrap working for the index

--- myapp/app.js
+++ myapp/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bootstrap.serve);
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
